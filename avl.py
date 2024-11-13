827f4396c39c3b279f2007ee0ac0aebbe9313c5a
wyatt-avilla <wyattmurphy1@gmail.com>
2024-11-12 16:25:13 -0800

    `avl.py` skeleton

---
+++ avl.py
@@ -0,0 +1,22 @@
+class AVLTree:
+    def __init__(self) -> None:
+        """
+        Initialize member variables for your `AVLTree` class here.
+        """
+        pass
+
+    def insert(self, val: str) -> None:
+        """
+        Inserts a string `val` into the AVL tree. If `val` is already present,
+        it will not be added again to ensure that each word appears only once.
+        """
+
+        pass
+
+    def count_in_range(self, lower_bound: str, upper_bound: str) -> int:
+        """
+        Count the number of strings in the data structure that are
+        lexicographically between `lower_bound`and `upper_bound`.
+        """
+
+        return -1
